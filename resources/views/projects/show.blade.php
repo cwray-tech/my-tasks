d488c14acc98a1df71d9112f0217eef4812f6816
Christopher Wray <53663762+cwray-tech@users.noreply.github.com>
2020-05-16 12:33:49 -0600

    added add task form to project show page

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -11,6 +11,22 @@
                 <a href="/tasks/create" class="btn btn-outline-primary ml-auto">New Task</a>
             </div>
             @include('.tasks.partials._table')
+            <h2 class="h2">Create a New Task</h2>
+            <div class="card mt-3">
+                <form class="card-body" action="/tasks" method="POST">
+                    @csrf
+                    <div class="form-group">
+                        <label for="name">Task</label>
+                        <input type="text" class="form-control" id="name" name="name" aria-describedby="nameHelp" value="{{ old('name') }}">
+                    </div>
+                    <input type="hidden" name="project" value="{{ $project->id }}">
+                    <div class="form-group form-check mt-4">
+                        <input type="checkbox" class="form-check-input" name="completed" id="completed">
+                        <label class="form-check-label" for="completed">Are you finished with this task?</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Add Task!</button>
+                </form>
+            </div>
         </div>
     </div>
 @endsection
